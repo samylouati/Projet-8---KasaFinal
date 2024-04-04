f893331effd086d48372bc69aed2fcfc149acf40
samylouati <148778977+samylouati@users.noreply.github.com>
2024-04-04 11:12:56 +0200

    css general revu

--- src/components/collapses/collapses.jsx
+++ src/components/collapses/collapses.jsx
@@ -1,19 +1,14 @@
 import React, {useState} from "react"; //Pour gerer l'etat du composant
 
 export function Collapses(props) {
-    const [isExpanded, setIsExpanded] = useState(false); //Etat pour controler l'affichage des <p>
-
-    const changeExpanded = () => {
-        setIsExpanded(!isExpanded); //Inversion de l'etat de <p>
-    }
   
     return (
         <div className="collapses">
-            <div className="collapses_content" onClick={changeExpanded}>
+            <div className="collapses_content">
                 <h2>{props.subject}</h2>
-                <i className={"fa-solid fa-angle-up"}></i>
+                <i className="fa-solid fa-angle-up"></i>
             </div>
-            <p className={isExpanded ? "visible" : "hidden"}>{props.text}</p> 
+            <p>{props.text}</p> 
         </div>
     );
 }
