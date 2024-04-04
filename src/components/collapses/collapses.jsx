bd54ca642ce1a8c45faa8e130bac2207955e4d5b
samylouati <148778977+samylouati@users.noreply.github.com>
2024-04-04 16:24:08 +0200

    rotation i encore à terminer

--- src/components/collapses/collapses.jsx
+++ src/components/collapses/collapses.jsx
@@ -1,17 +1,24 @@
-import React, {useState} from "react"; //Pour gerer l'etat du composant
+import React, { useState } from "react"; //Pour gerer l'etat des collapses
 
 export function Collapses(props) {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleCollapse = () => {
+        setCollapsed(!collapsed);
+    };  
     
   
     return (
         <div className="collapses">
-            <div className="collapses_content">
+            <div className="collapses_content" onClick={toggleCollapse}>
                 <h2>{props.subject}</h2>
-                <i className="fa-solid fa-angle-up"></i>
+                <i className='fa-solid fa-angle-up'></i>
             </div>
-            <div className="textContent ">
-                <p>{props.text}</p>
-            </div>
+            {!collapsed && (
+                <div className="textContent">
+                    <p>{props.text}</p>
+                </div>
+            )}
         </div>
     );
 }
