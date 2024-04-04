480568b52c432aa4e4008527701cf7cdd3e3dac4
samylouati <148778977+samylouati@users.noreply.github.com>
2024-04-04 11:28:04 +0200

    maj collapses

--- src/components/collapses/collapses.jsx
+++ src/components/collapses/collapses.jsx
@@ -1,14 +1,19 @@
 import React, {useState} from "react"; //Pour gerer l'etat du composant
 
 export function Collapses(props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleCollapse = () => {
+        setIsOpen(!isOpen);
+    };
   
     return (
         <div className="collapses">
-            <div className="collapses_content">
+            <div className="collapses_content" onClick={toggleCollapse}>
                 <h2>{props.subject}</h2>
-                <i className="fa-solid fa-angle-up"></i>
+                <i className={`fa-solid ${isOpen ? 'fa-angle-down' : 'fa-angle-up'}`}></i>
             </div>
-            <p>{props.text}</p> 
+            <p className={isOpen ? 'open' : 'closed'}>{props.text}</p> 
         </div>
     );
 }
