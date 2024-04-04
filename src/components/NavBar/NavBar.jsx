e46558754a0d276efae2a91cba9ea8a656c29856
samylouati <148778977+samylouati@users.noreply.github.com>
2024-04-04 10:14:33 +0200

    class active navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export function NavBar () {
   
     return (
             <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">A propos</Link>
+                <NavLink exact to="/" activeClassName="active">Accueil</NavLink>
+                <NavLink to="/about" activeClassName="active">A propos</NavLink>
             </nav>
     );
 }
+
+//"exact" indique que le lien doit etre actif lorsque l'URL corespond "exactement" à to="/"
+//class active lorsque que "exactement" l'URL spécifié
