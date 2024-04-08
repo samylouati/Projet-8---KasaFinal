1074bf5c0c9c7be83cc697e06e4d46965d520eac
samylouati <148778977+samylouati@users.noreply.github.com>
2024-04-08 17:07:03 +0200

    collapses ok

--- src/components/collapses/collapses.jsx
+++ src/components/collapses/collapses.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react"; //Pour gerer l'etat des collapses
 
 export function Collapses(props) {
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const toggleCollapse = () => {
+        setIsExpanded(!isExpanded);
+    };
     
     return (
         <div className="collapses">
-            <div className="collapses_content">
+            <div className="collapses_content" onClick={toggleCollapse}>
                 <h2>{props.subject}</h2>
-                <i className='fa-solid fa-angle-up'></i>
+                <i className={`fa-solid fa-angle-down ${isExpanded ? '' : 'rotate180'}`}></i>
             </div>
-            <div className="textContent">
+            <div className={`textContent ${isExpanded ? 'visible' : 'hidden'}`}>
                 <p>{props.text}</p>
             </div>
         </div>
