fa4e19be0922d1b42ef69c3908fd9e6114b9193a
samylouati <148778977+samylouati@users.noreply.github.com>
2024-04-08 11:01:28 +0200

    ajout lien page d'accueil sur logo

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -1,12 +1,12 @@
-//ajouter une classe active pour la page active 
 import LOGO from '../../assets/Images/LOGO_header.png' //je met la source de l'image dans une variable pour la changer plus facilement
 import { NavBar } from '../../components/NavBar/NavBar';
+import { Link } from 'react-router-dom'
 
 export function Header () {
   
     return (
         <header>
-            <h1><img src={LOGO} alt="" /></h1>
+            <Link to="/"><h1><img src={LOGO} alt="" /></h1></Link>
             <NavBar />
         </header>
     );
