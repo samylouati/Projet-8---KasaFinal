211d007677c868ab5c09a614de751d1d5137da5c
samylouati <148778977+samylouati@users.noreply.github.com>
2024-04-08 16:19:43 +0200

    bases modif collapses

--- src/components/collapses/collapses.jsx
+++ src/components/collapses/collapses.jsx
@@ -1,24 +1,16 @@
 import React, { useState } from "react"; //Pour gerer l'etat des collapses
 
 export function Collapses(props) {
-    const [collapsed, setCollapsed] = useState(true);
-
-    const toggleCollapse = () => {
-        setCollapsed(!collapsed);
-    };  
     
-  
     return (
         <div className="collapses">
-            <div className="collapses_content" onClick={toggleCollapse}>
+            <div className="collapses_content">
                 <h2>{props.subject}</h2>
                 <i className='fa-solid fa-angle-up'></i>
             </div>
-            {!collapsed && (
-                <div className="textContent">
-                    <p>{props.text}</p>
-                </div>
-            )}
+            <div className="textContent">
+                <p>{props.text}</p>
+            </div>
         </div>
     );
 }
